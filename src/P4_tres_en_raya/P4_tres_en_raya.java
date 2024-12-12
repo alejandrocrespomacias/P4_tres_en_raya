59dd4d4305c410925aef55abd21a3b0f94083f18
alejandrocrespomacias <128037138+alejandrocrespomacias@users.noreply.github.com>
2024-12-12 10:28:57 +0100

    Commit1
    Importo el Random

---
+++ src/P4_tres_en_raya/P4_tres_en_raya.java
@@ -0,0 +1,16 @@
+package P4_tres_en_raya;
+
+import java.util.Scanner;
+import java.util.Random;
+
+
+public class P4_tres_en_raya {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+	}
+
+}
